06ee4f3ce12f9e9ff1fc6d0ef6cde20c1f7ba18a
Owin Gunawardena <62776367+DuODude@users.noreply.github.com>
2021-03-24 22:22:11 +0530

    okay! added new stuff

---
+++ js/queries.js
@@ -0,0 +1,27 @@
+function preview_form(fname, eMail, description, subject) {
+    document.getElementById("form_content").innerHTML = "<p>Name: <span>" + fname + "</span><br>Email Address: <span>" + eMail + "</span><br>Subject: <span>" + subject + "</span><br>Details: <div id='description'>" + description + "</div></p>";
+    document.getElementById("preview_form").style.display ="block";
+    document.forms["qform"].style.display = "none";
+}
+
+function checkInputs(preview_form) {
+    var fname = document.forms["qform"]["fname"].value;
+    var eMail = document.forms["qform"]["eMail"].value;
+    var description = document.forms["qform"]["description"].value;
+    var subject = document.querySelector('input[name="theme"]:checked');
+
+    if (fname == "") {
+        alert("Please enter your full name");
+        fname.focus();
+    } else if (eMail == "") {
+        alert("Please enter your email address");
+        eMail.focus();
+    } else if (subject == null) {
+        alert("Please select a query subject");
+    } else if (description.value == "") {
+        alert("Enter details of your query");
+        description.focus();
+    } else {
+        preview_form(fname, eMail, description, subject.value);
+    }
+}
